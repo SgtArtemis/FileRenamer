6aaecf61151e2d1d0f50f5892e5d9d55c7ebedd7
Marcus Heine <football_macqan@hotmail.com>
2013-02-14 22:28:40 +0100

    Updated it to skip the EpNameWriter class

--- RenamerForGUI.java
+++ RenamerForGUI.java
@@ -12,8 +12,13 @@
 import java.io.*;
 import java.util.*;
 
+import javax.swing.JOptionPane;
+
 public class RenamerForGUI {
-	
+
+	EpNameLister lister;
+	ArrayList<String> episodeNames;
+
 	/**
 	 * 
 	 * Main method. This method isn't really supposed to be called, it's main use is testing.
@@ -21,10 +26,10 @@ public class RenamerForGUI {
 	 */
 	public static void main(String [] args) throws IOException {
 		RenamerForGUI renamer = new RenamerForGUI();
-		renamer.runRenamer("G:\\Serier\\", "Game of Thrones", 1);
+		renamer.runRenamer("G:\\Serier\\", "Sherlock", 1);
 	}
-	
-	
+
+
 	/**
 	 * The main method that's going to be used.
 	 * 
@@ -38,138 +43,162 @@ public class RenamerForGUI {
 	 * @param showNumber - The season number.
 	 */
 	public void runRenamer(String directoryString, String series, int showNumber) throws IOException {
-		
+
 		//Create the lister and the writer.
-		EpNameLister lister = new EpNameLister();
-		EpNameWriter writer = new EpNameWriter();
-		
-		//Create a new list, which will hold the names of the episodes.
-		ArrayList<String> episodeNames = new ArrayList<String>();	
-		
+		lister = new EpNameLister();
+		//EpNameWriter writer = new EpNameWriter();
+
 		int seriesNumber = showNumber;
-		
+
 		String dir = directoryString;
-		
-		String directoryForTextFile = directoryString + series;
-		
+
+		//String directoryForTextFile = directoryString + series;
+
 		dir = dir.concat(series + "\\Season " + seriesNumber + "\\");
-		
+
 		System.out.println("The directory in which the episodes are found is: " + dir);
-		
+
 		String season = "";
 		String episode = "";
 		String episodeName = "";
 		String fileEnding = "";
-		
-		
+
+
 		File directory = new File(dir);
-		
+
 		//we want the Season number to be S00 (as in S01 and S12) regardless of number
 		if(seriesNumber < 10)
 			season = " - S0" + seriesNumber;
 		else
 			season = " - S" + seriesNumber;
-		
-		
+
+
 		//Run the lister - Essentially meaning create a list of the episodes.
 		lister.run(series, seriesNumber);
-		
+
 		//Retrieve the list of episodes.
 		episodeNames = lister.getEpisodeNames();
-		
+
 		//If the list is empty, we assume that there are no episodes.
 		try {
 			System.out.println(episodeNames.get(0));
 		}
 		catch(IndexOutOfBoundsException error) {
-				System.out.println("Error: There are no episodes.");
-			}
-		
-		//Create the text file. See the documentation for writeText();
-		writer.writeText(series, directoryForTextFile, episodeNames);
-		
-		//Scan the text file we created.
-		Scanner sc = new Scanner(new File(directoryForTextFile + "\\" + series + ".txt"));
-
-		ArrayList<String> names = new ArrayList<String>();
-		
-		/*
-		 * Read the names of the episodes off the text file we create with EpNameWriter
-		 */
-		while(sc.hasNextLine()) {
-			names.add(sc.nextLine());
-			//System.out.println("Added some files.");
+			System.out.println("Error: There are no episodes.");
 		}
-		
-		sc.close();
-		
+
+
+		//Scan the text file we created.
+		//Scanner sc = new Scanner(new File(directoryForTextFile + "\\" + series + ".txt"));
+
+
 		/*
 		 * Create a "filter" for our files, overriding the class' accept method.
 		 */
+		// TODO  - Make the file ending support folders as well
 		FilenameFilter filefilter = new FilenameFilter()
-   		{
+		{
 			public boolean accept(File dir, String name)
-     		{
-     			//In case the filename ends with a (recognized) suffix, we return true.
+			{
+				//In case the filename ends with a (recognized) suffix, we return true.
 				if(name.endsWith(".avi") || name.endsWith(".mp4") || name.endsWith(".mkv") || name.endsWith(".mkv"))
 					return true;
-				
+
+				if(dir.isDirectory()) {
+					System.out.println("Dirname: " + dir);
+					return true;
+				}
+
+
 				else
 					return false;
 
-      		}
-    	};
-    	
-    	//Create a string array of all the files, only including the ones specified by the filter
-    	String [] filenames = directory.list(filefilter);
-    	
-    	if(filenames != null) {
-    	
-    	for(int i = 0; i < filenames.length; i++){
-    		
-    		//Since we want the format to be Exx (as in E01 or E14) regardless of episode number, we set up an if-else check.
-    		if(i<9)
-    			episode = "E0" + (i+1) + " - ";
-    		else
-    			episode = "E" + (i+1) + " - ";
-    		
-    		episodeName = names.get(i);
-    		
-    		// Old name of file
-    	    File oldName = new File(dir + filenames[i]);
-    	    
-    	    if((dir + filenames[i]).endsWith(".avi"))
-    	    	fileEnding = ".avi";
-    	    
-    	    else if((dir + filenames[i]).endsWith(".mp4"))
-    	    	fileEnding = ".mp4";
-    	    
-    	    else if((dir + filenames[i]).endsWith(".mov"))
-    	    	fileEnding = ".mov";
-    	    
-    	    else if((dir + filenames[i]).endsWith(".mkv"))
-    	    	fileEnding = ".mkv";
-    	    
-    	    else
-    	    	throw new IllegalArgumentException("File ending was not recognized.");
-    	    
-    	    //Define the new name of the file
-    	    File newName = new File(dir + series + season + episode + episodeName + fileEnding);
-    	    
-    	    //If the file name already exists, throw an exception.
-    	    if(newName.exists())
-    	    	throw new IOException("Another file with this name already exists.");
-    	    
-    	    
-    	    //Rename the file, and check whether or not it succeeded. 
-    	    if (!oldName.renameTo(newName)) {
-    	        System.out.println("Failed.");
-    	    }
-    	    else
-    	    	System.out.println("Succeeded.");
-    	} //End of for-loop
-    	
-	}	//End of if-statement
-	
+			}
+		};
+
+		//Create a string array of all the files, only including the ones specified by the filter
+		String [] filenames = directory.list(filefilter);
+
+		if(filenames != null) {
+
+			for(int i = 0; i < filenames.length; i++){
+
+				//Since we want the format to be Exx (as in E01 or E14) regardless of episode number, we set up an if-else check.
+				if(i<9)
+					episode = "E0" + (i+1) + " - ";
+				else
+					episode = "E" + (i+1) + " - ";
+
+				episodeName = episodeNames.get(i);
+
+				// Old name of file
+				File oldName = new File(dir + filenames[i]);
+
+				if((dir + filenames[i]).endsWith(".avi"))
+					fileEnding = ".avi";
+
+				else if((dir + filenames[i]).endsWith(".mp4"))
+					fileEnding = ".mp4";
+
+				else if((dir + filenames[i]).endsWith(".mov"))
+					fileEnding = ".mov";
+
+				else if((dir + filenames[i]).endsWith(".mkv"))
+					fileEnding = ".mkv";
+
+				else if(oldName.isDirectory())
+					fileEnding = "";
+
+				else
+					throw new IllegalArgumentException("File ending was not recognized.");
+
+				//Define the new name of the file
+				File newName = new File(dir + series + season + episode + episodeName + fileEnding);
+
+				//If the file name already exists, throw an exception.
+				if(newName.exists())
+					throw new IOException("Another file with this name already exists.");
+
+
+				//Rename the file, and check whether or not it succeeded. 
+				if (!oldName.renameTo(newName)) {
+					System.out.println("Failed.");
+				}
+				else
+					System.out.println("Succeeded.");
+			} //End of for-loop
+
+		}	//End of if-statement
+
+	}
+
+
+	public void deleteFile(String series, String directory) {
+
+		File file = new File(directory + "\\" + series + ".txt");
+		System.out.println(directory + "\\" + series + ".txt");
+
+		try{
+
+
+			if(file.delete()){
+				JOptionPane.showMessageDialog(null, "To name the episodes we created a temporary text file.\nThis text file has now been successfully deleted.");
+			}
+
+			else{
+				JOptionPane.showMessageDialog(null, "Delete operation failed. This doesn't really matter though.");
+			}
+
+		}
+		catch(Exception e){
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public int getListSize() {
+		return lister.getSize();
 	}
 }
